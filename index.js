02356b0a3cc3ce68539be2aa54aec0fe0c78dacf
Paul Salaets <psalaets@gmail.com>
2014-11-13 19:24:41 -0500

    circle follows mouse

--- index.js
+++ index.js
@@ -1,5 +1,26 @@
-console.log('check check');
-
 var createjs = require('createjs');
+var document = require('window').document;
 
-console.log('createjs has ' + Object.keys(createjs));
+var canvas = document.getElementById('stage');
+
+var stage = new createjs.Stage(canvas);
+
+var s = new createjs.Shape();
+s.x = 100;
+s.y = 100;
+
+var g = s.graphics;
+g.setStrokeStyle(1);
+g.beginStroke(createjs.Graphics.getRGB(0,0,0));
+g.beginFill(createjs.Graphics.getRGB(255,0,0));
+g.drawCircle(0, 0, 20);
+
+stage.addChild(s);
+
+createjs.Ticker.setFPS(60);
+createjs.Ticker.addEventListener('tick', function(event) {
+  s.x = stage.mouseX;
+  s.y = stage.mouseY;
+
+  stage.update();
+});
