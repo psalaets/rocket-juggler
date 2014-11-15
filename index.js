c5541adb5c5a82b468a8c4767462b413f3023931
Paul Salaets <psalaets@gmail.com>
2014-11-14 22:09:57 -0500

    add wall type

--- index.js
+++ index.js
@@ -7,17 +7,27 @@ var stage = new createjs.Stage(canvas);
 var p2 = require('p2');
 
 var Ball = require('./lib/ball');
-
+var Wall = require('./lib/wall');
 
 var world = new p2.World({
-  gravity: [5, 180]
+  gravity: [0, 180]
 });
 
 var objects = [];
 
-objects.push(createBall(10, 10, 20));
-objects.push(createBall(100, 100, 50));
-objects.push(createBall(200, 200, 5));
+objects.push(createBall(100, 100, 20));
+objects.push(createBall(200, 200, 50));
+objects.push(createBall(303, 0, 50));
+objects.push(createBall(300, 300, 50));
+
+var wallWidth = 20;
+var floorHeight = 20;
+
+var left = createWall(0, 0, wallWidth, 768 - floorHeight);
+var right = createWall(1024 - wallWidth, 0, wallWidth, 768 - floorHeight);
+var floor = createWall(0, 768 - floorHeight, 1024, floorHeight);
+
+objects.push(left, right, floor);
 
 createjs.Ticker.setFPS(30);
 createjs.Ticker.addEventListener('tick', function(event) {
@@ -38,4 +48,13 @@ function createBall(x, y, radius) {
   world.addBody(ball.body);
 
   return ball;
+}
+
+function createWall(x, y, width, height) {
+  var wall = new Wall(x, y, width, height);
+
+  stage.addChild(wall);
+  world.addBody(wall.body);
+
+  return wall;
 }
