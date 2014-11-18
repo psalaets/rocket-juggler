eabf6b336531fb3b654fbfb9076c6032b8ad1a72
Paul Salaets <psalaets@gmail.com>
2014-11-18 17:57:34 -0500

    organize file structure a bit

--- index.js
+++ index.js
@@ -2,8 +2,7 @@ var document = require('window').document;
 var canvas = document.getElementById('stage');
 
 var Game = require('./lib/game');
-var gameplay = require('./lib/gameplay');
-var title = require('./lib/title');
+var title = require('./lib/state/title');
 
 var game = new Game(canvas);
 game.changeState(title);
