13841813644e03725c995239998149d6da8c5fb1
Paul Salaets <psalaets@gmail.com>
2014-11-22 02:35:59 -0500

    start on prod build task

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,12 @@ var gulp = require('gulp'),
     browserify = require('browserify'),
     watchify = require('watchify'),
     source = require('vinyl-source-stream'),
-    browserSync = require('browser-sync')
+    browserSync = require('browser-sync'),
+    useref = require('gulp-useref'),
+    filter = require('gulp-filter'),
+    uglify = require('gulp-uglify'),
+    rev = require('gulp-rev'),
+    inject = require('gulp-inject')
 
 gulp.task('watch-commonjs', function() {
   var bundler = browserify({
@@ -44,5 +49,42 @@ gulp.task('dev', ['watch-commonjs', 'browser-sync-server'], function() {
     'app/index.html'
   ], {}, browserSync.reload);
 });
+
+gulp.task('prep-scripts', function() {
+  var jsFilter = filter('**/*.js');
+
+  return gulp.src('app/index.html')
+    // read files between build/endbuild marker comments and concat them into
+    // file with name specified by marker comment
+    .pipe(useref.assets())
+    // filter that down to just js files (there's no css yet so this isn't
+    // necessary right now)
+    .pipe(jsFilter)
+    // minify
+    .pipe(uglify())
+    // assign unique filename based on content
+    .pipe(rev())
+    .pipe(gulp.dest('build'));
+});
+
+gulp.task('prep-html', ['prep-scripts'], function() {
+  var vendorFile = gulp.src('build/vendor-*.js', {read: false});
+  var mainFile = gulp.src('build/main-*.js', {read: false});
+
+  return gulp.src('app/index.html')
+    .pipe(inject(vendorFile, {
+      addRootSlash: false,
+      ignorePath: 'build/',
+      name: 'inject-vendor'
+    }))
+    .pipe(inject(mainFile, {
+      addRootSlash: false,
+      ignorePath: 'build/',
+      name: 'inject-main'
+    }))
+    .pipe(gulp.dest('build'));
+});
+
+gulp.task('prod', ['prep-html']);
 
 gulp.task('default', ['dev']);
