13295a4aa28e665986e0341f09120af773f93495
Paul Salaets <psalaets@gmail.com>
2014-11-24 20:55:34 -0500

    persist config values when hitting save

--- app/lib/config/config-panel.js
+++ app/lib/config/config-panel.js
@@ -1,16 +1,20 @@
 var ko = require('knockout');
 
 var gameConfig = require('./game-config');
+var configPersistence = require('./persistence');
 
 var configKeys = gameConfig.keys();
 
 var viewModel = {
-  // apply view model's values to game config
-  updateGameConfig: function() {
+  saveGameConfig: function() {
+    // apply view model's values to game config
     configKeys.forEach(function(key) {
       var rawValue = this[key]();
       gameConfig.set(key, asNumber(rawValue));
     }, this);
+
+    // and persist those values
+    configPersistence.save(gameConfig.values());
   }
 };
 
