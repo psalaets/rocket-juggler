112219f2660d11d504756a61c90e90a76fddefe3
Paul Salaets <psalaets@gmail.com>
2014-11-25 20:04:30 -0500

    give player a rounded top

--- app/lib/entity/player.js
+++ app/lib/entity/player.js
@@ -16,7 +16,19 @@ function createView(x, y, width, height) {
   var g = new createjs.Graphics();
 
   g.beginFill('#000');
-  g.drawRect(-width / 2, -height / 2, width, height);
+
+  /**
+  * Draw rectangle with rounded top (using a circle to make rounded top)
+  */
+
+  var radius = width / 2;
+  // y value for top edge of player, relative to player center
+  var topOffset = -height / 2;
+  // x value for left edge of player, relative to player center
+  var leftOffset = -width / 2;
+
+  g.drawCircle(0, topOffset + radius, radius);
+  g.drawRect(topOffset + radius, leftOffset, width, height);
 
   var shape = new createjs.Shape(g);
   shape.x = x;
@@ -26,7 +38,8 @@ function createView(x, y, width, height) {
 
 function createBody(x, y, width, height) {
   var body = new p2.Body({
-    mass: 1,
+    // can't collide with it, only moves when its velocity is manually changed
+    type: p2.Body.KINEMATIC,
     position: [x, y]
   });
 
@@ -36,8 +49,17 @@ function createBody(x, y, width, height) {
   // doesn't spin
   body.fixedRotation = true;
 
-  body.addShape(new p2.Rectangle(width, height));
+  var halfHeight = height / 2;
+  var radius = width / 2;
+
+  body.addShape(new p2.Circle(radius), [0, radius - halfHeight]);
+  body.addShape(new p2.Rectangle(width, height - radius, [0, -radius / 2]));
   return body;
 }
 
 var p = Player.prototype;
+
+p.update = function(tickEvent) {
+  this.view.x = this.body.position[0];
+  this.view.y = this.body.position[1];
+};
