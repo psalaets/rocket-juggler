a17a02f3594d2fc1c63c8f2c03782d737aa738d7
Paul Salaets <psalaets@gmail.com>
2015-06-10 21:05:41 -0400

    clean up

--- app/index.js
+++ app/index.js
@@ -16,20 +16,20 @@ game.defineState('gameplay', require('./lib/state/gameplay'));
 
 var loader = require('./lib/loader');
 // gameplay state
-loader.loadImage('torso', 'assets/torso.png');
-loader.loadImage('legs', 'assets/legs.png');
-loader.loadImage('background', 'assets/background.png');
-loader.loadImage('rocket', 'assets/rocket.png');
-loader.loadImage('meteor', 'assets/meteor.png');
+loader.loadImage('torso',               'assets/torso.png');
+loader.loadImage('legs',                'assets/legs.png');
+loader.loadImage('background',          'assets/background.png');
+loader.loadImage('rocket',              'assets/rocket.png');
+loader.loadImage('meteor',              'assets/meteor.png');
 // title state
-loader.loadImage('about-button', 'assets/aboutbutton.png');
-loader.loadImage('about-button-hover', 'assets/aboutbuttonhover.png');
-loader.loadImage('play-button', 'assets/playbutton.png');
-loader.loadImage('play-button-hover', 'assets/playbuttonhover.png');
-loader.loadImage('title-screen', 'assets/withoutbuttons.png');
+loader.loadImage('about-button',        'assets/aboutbutton.png');
+loader.loadImage('about-button-hover',  'assets/aboutbuttonhover.png');
+loader.loadImage('play-button',         'assets/playbutton.png');
+loader.loadImage('play-button-hover',   'assets/playbuttonhover.png');
+loader.loadImage('title-screen',        'assets/withoutbuttons.png');
 
+// start game after everything is loaded
 loader.on('ready', function() {
-  // start game after everything is loaded
   game.changeState('title');
   game.start();
 });
