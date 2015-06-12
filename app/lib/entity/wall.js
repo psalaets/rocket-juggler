472cb83bb21219e560943730f4c16a77f629ab26
Paul Salaets <psalaets@gmail.com>
2015-06-11 20:20:25 -0400

    use planes for walls

--- app/lib/entity/wall.js
+++ app/lib/entity/wall.js
@@ -3,31 +3,31 @@ var p2 = require('p2');
 
 module.exports = Wall;
 
-function Wall(x, y, width, height) {
-  //this.view = createView(width, height);
-  //this.view.x = x;
-  //this.view.y = y;
-
-  this.body = createBody(x, y, width, height);
+function Wall(position, type) {
+  this.body = createBody(position, type);
 }
 
-function createView(width, height) {
-  var g = new createjs.Graphics();
-
-  g.beginFill('#000000');
-  g.drawRect(0, 0, width, height);
-
-  return new createjs.Shape(g);
-}
-
-function createBody(x, y, width, height) {
+function createBody(position, type) {
   var body = new p2.Body({
     // mass = 0 makes it static body
-    mass: 0,
-    // x and y in p2 are center of shape, so adjust for that
-    position: [x + (width / 2), y + (height / 2)]
+    mass: 0
   });
-  body.addShape(new p2.Rectangle(width, height));
+
+  body.addShape(new p2.Plane());
+
+  if (type == 'bottom') {
+    body.angle = Math.PI;
+    body.position = [0, position];
+  } else if (type == 'right') {
+    body.angle = Math.PI / 2;
+    body.position = [position, 0];
+  } else if (type == 'left') {
+    body.angle = (3 * Math.PI) / 2;
+    body.position = [position, 0];
+  } else if (type == 'top') {
+    body.angle = 0;
+    body.position = [0, position];
+  }
 
   return body;
 }
